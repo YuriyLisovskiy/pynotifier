7e3c719cbac6b47a3cea1f398a814364ef2965f8
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-12-28 15:01:11 +0200

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 python3 -m pip install --user --upgrade setuptools wheel
 python3 setup.py sdist bdist_wheel
 python3 -m pip install --user --upgrade twine
-twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+twine upload dist/*
