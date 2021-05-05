098e483f12c9eee115bcb7fef84c5018513484c9
Michael Joseph <michaeljoseph+github@gmail.com>
2021-05-05 23:30:08 +0200

    Apply automated pre-commit fixes

--- pynotifier/pynotifier.py
+++ pynotifier/pynotifier.py
@@ -4,7 +4,7 @@
 # Distributed under the MIT licence,
 # see the accompanying file LICENSE.
 
-__all__ = ['Notification']
+__all__ = ["Notification"]
 
 import platform
 import shutil
@@ -12,82 +12,103 @@ import subprocess
 
 
 class Notification:
-	"""
-	'app_name' - name of app sending notification (for linux only)
-	'title' - a title of notification.
-	'description' - more info about the notification.
-	'duration' - notification timeout in seconds.
-	'urgency' - notification urgency level (ignored under Windows);
-				possible values: 'low', 'normal', 'critical'.
-	'icon_path' - path to notification icon file.
-	"""
-	def __init__(self, title, description='', duration=5, urgency='low', icon_path=None, app_name=None):
-		if title is None:
-			raise ValueError('title with None value is not allowed')
+    """
+    'app_name' - name of app sending notification (for linux only)
+    'title' - a title of notification.
+    'description' - more info about the notification.
+    'duration' - notification timeout in seconds.
+    'urgency' - notification urgency level (ignored under Windows);
+                            possible values: 'low', 'normal', 'critical'.
+    'icon_path' - path to notification icon file.
+    """
 
-		if title == '':
-			raise ValueError('title must not be empty')
+    def __init__(
+        self,
+        title,
+        description="",
+        duration=5,
+        urgency="low",
+        icon_path=None,
+        app_name=None,
+    ):
+        if title is None:
+            raise ValueError("title with None value is not allowed")
 
-		system = platform.system().lower()
+        if title == "":
+            raise ValueError("title must not be empty")
 
-		platforms = ['windows', 'linux', 'darwin']
-		if system not in platforms:
-			raise SystemError('notifications are not supported for {} system'.format(system))
+        system = platform.system().lower()
 
-		if system == 'linux' and urgency not in ['low', 'normal', 'critical', None]:
-			raise ValueError('invalid urgency was given: {}'.format(urgency))
+        platforms = ["windows", "linux", "darwin"]
+        if system not in platforms:
+            raise SystemError(
+                "notifications are not supported for {} system".format(system)
+            )
 
-		self.system = system
-		self.__title = title
-		self.__description = description
-		self.__duration = duration
-		self.__urgency = urgency
-		self.__icon_path = icon_path
-		self.__app_name = app_name	
+        if system == "linux" and urgency not in ["low", "normal", "critical", None]:
+            raise ValueError("invalid urgency was given: {}".format(urgency))
 
-	def send(self):
-		# https://stackoverflow.com/questions/3951840/how-to-invoke-a-function-on-an-object-dynamically-by-name
-		getattr(self, f'send_{self.system}')()
+        self.system = system
+        self.__title = title
+        self.__description = description
+        self.__duration = duration
+        self.__urgency = urgency
+        self.__icon_path = icon_path
+        self.__app_name = app_name
 
-	def send_linux(self):
-		"""Notify on linux with notify-send"""
-		if shutil.which('notify-send') is None:
-			raise SystemError('Please install libnotify-bin\n\tsudo apt-get install libnotify-bin')
+    def send(self):
+        # https://stackoverflow.com/questions/3951840/how-to-invoke-a-function-on-an-object-dynamically-by-name
+        getattr(self, f"send_{self.system}")()
 
-		command = [
-			'notify-send', '{}'.format(self.__title),
-			'{}'.format(self.__description),
-			'-t', '{}'.format(self.__duration * 1000)
-		]
-		if self.__urgency is not None:
-			command += ['-u', self.__urgency]
+    def send_linux(self):
+        """Notify on linux with notify-send"""
+        if shutil.which("notify-send") is None:
+            raise SystemError(
+                "Please install libnotify-bin\n\tsudo apt-get install libnotify-bin"
+            )
 
-		if self.__icon_path is not None:
-			command += ['-i', self.__icon_path]
+        command = [
+            "notify-send",
+            "{}".format(self.__title),
+            "{}".format(self.__description),
+            "-t",
+            "{}".format(self.__duration * 1000),
+        ]
+        if self.__urgency is not None:
+            command += ["-u", self.__urgency]
 
-		if self.__app_name is not None:
-			command += ['-a', self.__app_name]
+        if self.__icon_path is not None:
+            command += ["-i", self.__icon_path]
 
-		subprocess.call(command)
+        if self.__app_name is not None:
+            command += ["-a", self.__app_name]
 
-	def send_windows(self):
-		"""Notify on windows with win10toast"""
-		try:
-			import win10toast
-			win10toast.ToastNotifier().show_toast(
-				threaded=True,
-				title=self.__title,
-				msg=self.__description,
-				duration=self.__duration,
-				icon_path=self.__icon_path
-			)
-		except ImportError:
-			raise ImportError('notifications are not supported, can\'t import win10toast')
+        subprocess.call(command)
 
-	def send_darwin(self):
-		"""Notify on macos with pync"""
-		try:
-			import pync
-			pync.notify(self.__description, title=self.__title, appIcon=self.__icon_path)
-		except ImportError:
-			raise ImportError('notifications are not supported, can\'t import pync')
+    def send_windows(self):
+        """Notify on windows with win10toast"""
+        try:
+            import win10toast
+
+            win10toast.ToastNotifier().show_toast(
+                threaded=True,
+                title=self.__title,
+                msg=self.__description,
+                duration=self.__duration,
+                icon_path=self.__icon_path,
+            )
+        except ImportError:
+            raise ImportError(
+                "notifications are not supported, can't import win10toast"
+            )
+
+    def send_darwin(self):
+        """Notify on macos with pync"""
+        try:
+            import pync
+
+            pync.notify(
+                self.__description, title=self.__title, appIcon=self.__icon_path
+            )
+        except ImportError:
+            raise ImportError("notifications are not supported, can't import pync")
--- setup.py
+++ setup.py
@@ -6,51 +6,56 @@ import os
 
 from setuptools import find_packages, setup
 
+NAME = "py-notifier"
+DESCRIPTION = "Cross-platform desktop push-notifications."
+URL = "https://github.com/YuriyLisovskiy/pynotifier"
+EMAIL = "yuralisovskiy98@gmail.com"
+AUTHOR = "Yuriy Lisovskiy"
+REQUIRES_PYTHON = ">=3.6.0"
+VERSION = (0, 3, 0)
 
-NAME = 'py-notifier'
-DESCRIPTION = 'Cross-platform desktop push-notifications.'
-URL = 'https://github.com/YuriyLisovskiy/pynotifier'
-EMAIL = 'yuralisovskiy98@gmail.com'
-AUTHOR = 'Yuriy Lisovskiy'
-REQUIRES_PYTHON = '>=3.6.0'
+REQUIRED = [
+    "win10toast==0.9; platform_system=='Windows'",
+    "pync==2.0.3; platform_system=='Darwin'",
+]
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'VERSION')) as ver:
-	VERSION = tuple([int(x) for x in ver.read(-1).split('.')])
+with open(os.path.join(here, "VERSION")) as ver:
+    VERSION = tuple([int(x) for x in ver.read(-1).split(".")])
 
-with open(os.path.join(here, 'requirements.txt')) as txt:
-	REQUIRED = [module_name.rstrip('\n') for module_name in txt.readlines()]
+with open(os.path.join(here, "requirements.txt")) as txt:
+    REQUIRED = [module_name.rstrip("\n") for module_name in txt.readlines()]
 
 try:
-	with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-		long_description = '\n' + f.read()
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
 except FileNotFoundError:
-	long_description = DESCRIPTION
+    long_description = DESCRIPTION
 
 setup(
-	name=NAME,
-	version='.'.join(map(str, VERSION)),
-	description=DESCRIPTION,
-	long_description=long_description,
-	long_description_content_type='text/markdown',
-	author=AUTHOR,
-	author_email=EMAIL,
-	python_requires=REQUIRES_PYTHON,
-	url=URL,
-	packages=find_packages(exclude=('tests',)),
-	install_requires=REQUIRED,
-	include_package_data=True,
-	license='MIT',
-	classifiers=[
-		# Full list: https://pypi.org/classifiers
-		'License :: OSI Approved :: MIT License',
-		'Programming Language :: Python',
-		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.6',
-		'Environment :: MacOS X',
-		'Operating System :: MacOS',
-		'Operating System :: POSIX :: Linux',
-		'Operating System :: Microsoft :: Windows :: Windows 10'
-	]
+    name=NAME,
+    version=".".join(map(str, VERSION)),
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=("tests",)),
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license="MIT",
+    classifiers=[
+        # Full list: https://pypi.org/classifiers
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Environment :: MacOS X",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+    ],
 )
