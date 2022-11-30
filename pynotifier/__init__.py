b5d9fb641a3a3c10782ab892b0822d52fad58fb7
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2022-11-30 16:24:26 +0200

    updated docs

--- pynotifier/__init__.py
+++ pynotifier/__init__.py
@@ -2,6 +2,8 @@
 Send notifications using backends.
 Supported backends can be found in 'pynotifier.backends' module.
 
+This file is a part of py-notifier project.
+
 Copyright (c) 2018, 2021-2022 Yuriy Lisovskiy
 
 Distributed under the MIT licence, see the accompanying file LICENSE.
--- pynotifier/notification.py
+++ pynotifier/notification.py
@@ -1,4 +1,6 @@
 """
+This file is a part of py-notifier project.
+
 Copyright (c) 2018, 2021-2022 Yuriy Lisovskiy
 
 Distributed under the MIT licence, see the accompanying file LICENSE.
