fa2aa9391d84a19fd16605f7c962737fa8b0a89e
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2023-02-19 18:41:19 +0200

    Add SMTPBackend for mass mails

--- pynotifier/backends/smtp.py
+++ pynotifier/backends/smtp.py
@@ -14,45 +14,69 @@ from pynotifier.notification import Notification
 from .backend import NotificationBackend
 
 
+class DSMTP_SSL(smtplib.SMTP_SSL):
+	"""DSMTP_SSL - SMTP protocol with default SSL context and port.
+
+	"""
+	SSL_PORT = 465
+
+	def __init__(self, host, *args, **kwargs):
+		super().__init__(host, self.SSL_PORT, context=ssl.create_default_context(), *args, **kwargs)
+
+
 class SMTPBackend(NotificationBackend):
 	"""SMTP notification backend.
+	mime_type: [str], default: "plain"
+
+	Additional parameters for Notification.Config object:
+	 - emails: [str]
+
+	Note: info about how to send email from Google Mail with App Password:
+	https://levelup.gitconnected.com/an-alternative-way-to-send-emails-in-python-5630a7efbe84
 
 	"""
 	IDENTIFIER = 'pynotifier.backends.smtp'
 
-	def __init__(self, email, password, *args, **kwargs):
-		"""TODO:
+	def __init__(self,
+	             server: smtplib.SMTP,
+	             email: str,
+	             password: str,
+	             mime_type: str = 'plain',
+	             *args,
+	             **kwargs):
+		"""Initialize the SMTPBackend.
+		If the SMTP server supports authentication, the backend will log in automatically.
 
 		"""
 		super().__init__(*args, **kwargs)
-		self._ssl_port = 465
+		self._server = server
 		self._email = email
 		self._password = password
-		self._ssl_context = ssl.create_default_context()
-		self._server = smtplib.SMTP_SSL("smtp.gmail.com", self._ssl_port, context=self._ssl_context)
-		self._server.login(self._email, self._password)
+		self._mime_type = mime_type
+
+		try:
+			self._server.login(self._email, self._password)
+		except smtplib.SMTPNotSupportedError:
+			pass
 
 	def notify(self, notification: Notification):
 		"""Send the notification using config from Notification object.
 
 		"""
-		receiver_email = notification.config.get('email', '')
-		message = self._get_message(notification, receiver_email)
-		self._server.sendmail(self._email, receiver_email, message.as_string())
+		to_emails = notification.config.get('emails', '')
+		for to_email in to_emails:
+			self._send_notification(to_email, notification)
 
-	def _get_message(self, notification: Notification, receiver_email: str):
-		"""Builds MIMEMultipart message from notification options.
-		Note: receiver_email is received from notification.config.
+	def _send_notification(self, to_email: str, notification: Notification):
+		message = self._get_message(notification, to_email)
+		self._server.sendmail(self._email, to_email, message.as_string())
 
-		"""
+	def _get_message(self, notification: Notification, to_email: str):
 		message = MIMEMultipart()
-		message["From"] = self._email
-		message["To"] = receiver_email
-		message["Subject"] = notification.title
+		message['Subject'] = notification.title
+		message['From'] = self._email
+		message['To'] = to_email
 
-		# Recommended for mass emails
-		message["Bcc"] = receiver_email
-
-		message.attach(MIMEText(notification.message, notification.config.get('mime_type', 'plain')))
+		message.attach(MIMEText(notification.message, self._mime_type))
 
 		return message
