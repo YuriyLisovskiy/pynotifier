71b35378ce9a755ec47d4a99cf88e746728beb53
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2023-03-09 23:22:55 +0200

    Add terumx-toast options

--- pynotifier/backends/platform/android.py
+++ pynotifier/backends/platform/android.py
@@ -16,6 +16,12 @@ from ..backend import NotificationBackend
 
 assert_system('android')
 
+TOAST_POSITION_TOP = 'top'
+TOAST_POSITION_MIDDLE = 'middle'
+TOAST_POSITION_BOTTOM = 'bottom'
+
+_AVAILABLE_TOAST_POSITIONS = [TOAST_POSITION_MIDDLE, TOAST_POSITION_MIDDLE, TOAST_POSITION_BOTTOM]
+
 
 class AndroidTermuxBackend(NotificationBackend):
 	"""
@@ -32,22 +38,29 @@ class AndroidTermuxBackend(NotificationBackend):
 
 		'title' - a title of notification
 		'message' - more info about the notification
-		TODO: add available params of notification and it's config...
+
+		Termux Toast config
+		'termux_toast':
+			'bg_color' - the background color (default: gray) (standard name, "#FF0000" or "#FFFF0000")
+			'text_color' - the text color (default: white) (standard name, "#FF0000" or "#FFFF0000")
+			'position' - the position of the toast [top, middle, bottom] (default: middle)
+			'show_short' - only show the toast for a short time
+
+		Termux Notification config:
+		'termux':
+			'' -
 		"""
-		command = []
 		if notification.config.get('toast', False):
 			command = [
 				self._get_cli_app('termux-toast'),
 				"\"{}: {}\"".format(notification.title, notification.message),
-			]
-			# TODO: Append command args...
+			] + self._build_toast_opts(notification.config.get('termux_toast', dict()))
 		else:
 			command = [
 				self._get_cli_app('termux-notification'),
 				"--title", "\"{}\"".format(notification.title),
 				"--content", "\"{}\"".format(notification.message),
-			]
-			# TODO: Append command args...
+			] + self._build_notification_opts(notification.config.get('termux', dict()))
 
 		subprocess.Popen(command, shell=False)
 
@@ -61,6 +74,39 @@ Visit the https://f-droid.org/en/packages/com.termux.api/ website to install the
 			)
 
 		return app
+
+	@staticmethod
+	def _build_toast_opts(config: dict):
+		opts = [
+			'-b', config.get('bg_color', 'gray'),
+			'-c', config.get('text_color', 'white')
+		]
+
+		show_short = _validate_opt_or_default(config.get('show_short', 'true'), ['true', 'false'], 'false')
+		opts += ['-s', show_short]
+
+		position = _validate_opt_or_default(
+			config.get('position', TOAST_POSITION_MIDDLE),
+			_AVAILABLE_TOAST_POSITIONS,
+			TOAST_POSITION_MIDDLE
+		)
+		opts += ['-g', position]
+
+		return opts
+
+	@staticmethod
+	def _build_notification_opts(config: dict):
+		opts = []
+		#TODO: add opts from doc: https://wiki.termux.com/wiki/Termux-notification
+
+		return opts
+
+
+def _validate_opt_or_default(opt: str, available_values: list, default: object):
+	if opt not in available_values:
+		return default
+
+	return opt
 
 
 Backend = AndroidTermuxBackend
